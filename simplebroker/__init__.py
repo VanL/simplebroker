1c9f01b495ea7d1d954fc7a16ecacf422f6686bb
VanL <van@ospo.co>
2025-08-06 10:53:12 -0500

    Update formatting

--- simplebroker/__init__.py
+++ simplebroker/__init__.py
@@ -2,10 +2,10 @@
 
 # Import main components
 # Import BrokerDB for backward compatibility (but don't export it)
+from ._constants import __version__
 from .db import BrokerDB as _BrokerDB  # noqa: F401
 from .sbqueue import Queue
 from .watcher import QueueMoveWatcher, QueueWatcher
-from ._constants import __version__
 
 # Only export the new API
 __all__ = ["Queue", "QueueWatcher", "QueueMoveWatcher", "__version__"]
