bf08717853ee9824badf4b6acfb2387e2c0064e2
VanL <van@ospo.co>
2025-10-24 14:44:26 -0500

    Add py.typed file

--- simplebroker/_constants.py
+++ simplebroker/_constants.py
@@ -34,7 +34,7 @@ from typing import Any, Final
 # VERSION INFORMATION
 # ==============================================================================
 
-__version__: Final[str] = "2.6.2"
+__version__: Final[str] = "2.7.0"
 """Current version of SimpleBroker."""
 
 # ==============================================================================
