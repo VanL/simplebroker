8c0fc1fe4f72f0e107f69b54153c89b7888d0597
VanL <van@ospo.co>
2026-05-05 11:33:14 -0500

    3.3.2 Release

--- simplebroker/_constants.py
+++ simplebroker/_constants.py
@@ -35,7 +35,7 @@ from typing import Any, Final
 # VERSION INFORMATION
 # ==============================================================================
 
-__version__: Final[str] = "3.3.1"
+__version__: Final[str] = "3.3.2"
 """Current version of SimpleBroker."""
 
 # ==============================================================================
