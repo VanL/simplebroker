20ab0b82cf49838cc4a3283051293ec7dfdb85ac
VanL <van@ospo.co>
2026-07-06 05:38:37 -0500

    Update versions

--- simplebroker/_constants.py
+++ simplebroker/_constants.py
@@ -35,7 +35,7 @@ from typing import Any, Final
 # VERSION INFORMATION
 # ==============================================================================
 
-__version__: Final[str] = "5.0.1"
+__version__: Final[str] = "5.0.2"
 """Current version of SimpleBroker."""
 
 # ==============================================================================
