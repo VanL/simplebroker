4575471fc1f19141b2d95387b7e0644b63dbc5ba
VanL <van@ospo.co>
2026-08-10 18:42:56 -0500

    Release simplebroker 7.0.1

--- simplebroker/_constants.py
+++ simplebroker/_constants.py
@@ -36,7 +36,7 @@ from typing import Any, Final
 # VERSION INFORMATION
 # ==============================================================================
 
-__version__: Final[str] = "7.0.0"
+__version__: Final[str] = "7.0.1"
 """Current version of SimpleBroker."""
 
 # ==============================================================================
